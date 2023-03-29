494958711d4597f6b824565c6a390dc85cc2ae43
Prasanna <prasannaacharya2073@gmail.com>
2023-03-29 07:56:37 +0545

    MVC architectre updated

---
+++ nodejs/MVC pattern/db/database.js
@@ -0,0 +1,4 @@
+const dbUrl =
+  "mongodb+srv://prasannaacharya2073:wH3mUwh2wGPQ0W0A@cluster0.fuzoins.mongodb.net/?retryWrites=true&w=majority";
+
+module.exports.dbUrl = dbUrl;
---
+++ nodejs/MVC pattern/models/todo.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: String,
+    status: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = { Todo };
