4233eb778b65d834944e67cebd5591460d79da68
Prasanna <prasannaacharya2073@gmail.com>
2023-03-29 08:12:36 +0545

    Controller and Routes updated in MVC pattern

---
+++ nodejs/MVC pattern/controllers/todoController.js
@@ -0,0 +1,57 @@
+const createTodo = (req, res) => {
+  console.log("REQUEST", req.body, req.params, req.query);
+  const todo = new Todo({
+    title: req.body.title,
+    status: req.body.status,
+  });
+  todo
+    .save()
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => res.send(error));
+};
+
+const getTodos = (req, res) => {
+  const todos = Todo.find();
+  todos
+    .then((response) => res.send(response))
+    .catch((error) => res.send(error));
+};
+
+const getTodo = (req, res) => {
+  const todoId = req.params.todoId;
+
+  const findTodo = Todo.find({ _id: todoId });
+
+  find.then((response) => res.send(response)).catch((error) => res.send(error));
+};
+
+const deleteTodo = (req, res) => {
+  const todoId = req.params.todoId;
+
+  const deleteTodo = Todo.deleteOne({ _id: todoId });
+  deleteTodo
+    .then((response) => res.send(response))
+    .catch((error) => res.send(error));
+};
+
+const updateTodo = (req, res) => {
+  const todoId = req.params.todoId;
+  console.log(todoId, req.body);
+  const updateTodo = Todo.findByIdAndUpdate(
+    {
+      _id: todoId,
+    },
+    {
+      title: req.body.title,
+      status: req.body.status,
+    },
+    {
+      new: true,
+    }
+  );
+  updateTodo
+    .then((response) => res.send(response))
+    .catch((error) => res.send(error));
+};
---
+++ nodejs/MVC pattern/routes/route.js
@@ -0,0 +1,16 @@
+const todoRoutes = (app) => {
+  // create single todo
+  app.post("/api/todos", createTodo);
+
+  // get all todos
+  app.get("/api/todos", getTodos);
+
+  // get single todo (read)
+  app.get("/api/todos/:todoId", getTodo);
+
+  // delete single todo (delete)
+  app.delete("/api/todos/:todoId", deleteTodo);
+};
+
+// put single todo (update)
+app.put("/api/todos/:todoId", updateTodo);
