4cfab302c5aecc1d5b87d0c92019baf74c8bcb74
Prasanna <prasannaacharya2073@gmail.com>
2023-03-30 11:44:52 +0545

    Data fetching issue from database fixed

--- nodejs/MVC-pattern/index.js
+++ nodejs/MVC-pattern/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { dbUrl } = require("./db/database");
 const { todoRoutes } = require("./routes/route");
 
 // middlewares
-app.use(cors);
+// app.use(cors);
 app.use(bodyParser.json());
 
 // database connection
